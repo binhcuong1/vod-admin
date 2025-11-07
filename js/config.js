5a3cc1317600d913f7928d90db1ed744d20a0d82
binhcuong <binhcuongpy2014@gmail.com>
2025-11-07 21:54:00 +0700

    Add tính năng quản lý phim & thêm phim lẻ

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // Đặt cấu hình dùng chung cho toàn admin
 window.API_BASE = "http://127.0.0.1:3000";   
-window.API_GENRE = "/api/genres";            
+window.API_GENRE = "/api/genres";  
+window.API_MOVIE = "/api/films";           
